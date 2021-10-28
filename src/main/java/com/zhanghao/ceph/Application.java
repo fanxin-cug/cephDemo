dc96dd2121c3680979afaa36aadbaf3181a238e1
fanxin <fanxin806@cug.edu.cn>
2021-10-28 18:21:50 +0800

    little change

--- src/main/java/com/zhanghao/ceph/Application.java
+++ src/main/java/com/zhanghao/ceph/Application.java
@@ -1,5 +1,6 @@
 package com.zhanghao.ceph;
 
+import com.zhanghao.ceph.config.GdalConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        for(String arg : args){
+            if(arg.contains("--gdal")){
+                String gdalPath = arg.substring(arg.indexOf("=")+1);
+                GdalConfig.init(gdalPath);
+            }
+        }
         SpringApplication.run(Application.class,args);
     }
 }
--- src/main/java/com/zhanghao/ceph/config/GdalConfig.java
+++ src/main/java/com/zhanghao/ceph/config/GdalConfig.java
@@ -8,9 +8,9 @@ public class GdalConfig {
 
     public static final String GDAL_LIB_PATH = "/lib/gdal";
 
-    public static boolean init(){
+    public static boolean init(String gdalPath){
         try{
-            GdalLibLoad.loadLinuxGdalLib(GDAL_LIB_PATH);
+            GdalLibLoad.loadLinuxGdalLib(gdalPath);
             gdal.SetConfigOption("GDAL_FILENAME_IS_GBK","YES");
             gdal.AllRegister();
             return true;
